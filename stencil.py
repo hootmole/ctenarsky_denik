707466073c12e36b3174b105723bd15ab505aff8
hootmole <jachym1137@gmail.com>
2022-12-30 17:40:59 +0100

    Update stencil.py

--- stencil.py
+++ stencil.py
@@ -3,47 +3,66 @@
 
 stencil = [
     """
-    Author: William Shakespeare
-    Illustrator: none
-    Translator: E. A. Saudek
-    Publisher: Mladá Fronta Praha
-    Page count: 116
+Author: William Shakespeare
+Illustrator: none
+Translator: E. A. Saudek
+Publisher: Mladá Fronta Praha
+Page count: 116
     """,
     """
-    William Shakespeare was an English playwright and poet who is widely considered
-    one of the greatest writers in the English language. He was born in April 1564, a time of great cultural and artistic flourishing in England.
-    He is credited with approximately 38 surviving plays (including some he may have collaborated on).
-    Shakespeare's plays are divided into three categories: comedies, tragedies, and histories. His comedies are known for their wit, humor, and charm, while his
-    tragedies explore deep and complex emotions such as love, jealousy, and betrayal. His histories depict the lives and deeds of English monarchs and other
-    historical figures. In 1616 he died of fever.
-    Shakespeare's plays and poems are still widely performed and read today, and his work has been translated into many languages.
+William Shakespeare was an English playwright and poet who is widely considered
+one of the greatest writers in the English language. He was born in April 1564, a time of great cultural and artistic flourishing in England.
+He is credited with approximately 38 surviving plays (including some he may have collaborated on).
+Shakespeare's plays are divided into three categories: comedies, tragedies, and histories. His comedies are known for their wit, humor, and charm, while his
+tragedies explore deep and complex emotions such as love, jealousy, and betrayal. His histories depict the lives and deeds of English monarchs and other
+historical figures. In 1616 he died of fever.
+Shakespeare's plays and poems are still widely performed and read today, and his work has been translated into many languages.
     """,
     """
-    The Renaissance was a period of great cultural and intellectual growth that took place in Europe during the 14th to 17th centuries. It is characterized by a renewed interest in the classical learning and values of ancient Greece and Rome, and is often referred to as the "rebirth" of classical civilization.
-    During the Renaissance, there was a focus on the individual and on human potential. This was reflected in the art, literature, and philosophy of the time, which celebrated human accomplishment and the beauty of the human form. The Renaissance also saw significant advances in science, technology, and exploration, as well as the growth of commerce and trade.
-    One of the key features of the Renaissance was the rediscovery and translation of the works of ancient Greek and Roman writers, which had a profound influence on the thinking and culture of the time. This renewed interest in classical learning led to a revival of the arts and sciences, and the development of new ideas in fields such as literature, art, architecture, music, and politics.
-    The Renaissance was also a time of great social and political change, as the feudal system that had dominated medieval Europe began to decline and new forms of government, such as absolute monarchies, emerged. The Renaissance was a period of great cultural and intellectual growth that had a lasting impact on the development of Western civilization.
+The Renaissance was a period of great cultural and intellectual growth that took place in Europe during the 14th to 17th centuries. It is characterized by a renewed interest in the classical learning and values of ancient Greece and Rome, and is often referred to as the "rebirth" of classical civilization.
+During the Renaissance, there was a focus on the individual and on human potential. This was reflected in the art, literature, and philosophy of the time, which celebrated human accomplishment and the beauty of the human form. The Renaissance also saw significant advances in science, technology, and exploration, as well as the growth of commerce and trade.
+One of the key features of the Renaissance was the rediscovery and translation of the works of ancient Greek and Roman writers, which had a profound influence on the thinking and culture of the time. This renewed interest in classical learning led to a revival of the arts and sciences, and the development of new ideas in fields such as literature, art, architecture, music, and politics.
+The Renaissance was also a time of great social and political change, as the feudal system that had dominated medieval Europe began to decline and new forms of government, such as absolute monarchies, emerged. The Renaissance was a period of great cultural and intellectual growth that had a lasting impact on the development of Western civilization.
     """,
     """
-    The story is about two descendants of Romeo and Juliet from families that hate each other - the Capulets and the Montagues.
-    It all starts with a ball organised by the Capulets, at which Juliet is to be betrothed to Paride.
-    Romeo hears the news and goes there. At the ball, however, he meets Tybalt.
-    Tybalt tries to provoke him into an argument, but is unsuccessful.
-    At the ball, Romeo meets Juliet, with whom he immediately falls in love.
-    When the ball ends, Romeo decides to visit Juliet in the Capulets' garden.
-    The next day, Tybalt kills Romeo's friend Mercutio and Romeo kills Tybalt in return.
-    Romeo is then banished from Verona, where the story takes place.
-    After some time, Romeo receives news that Juliet is going to marry the Parido.
-    Juliet is not given a choice and is forced into the marriage by her clan, so she devises a plan.
-    She therefore asks Father Lawrence to give her a potion, after drinking it she will look dead for a while and then Romeo will take her out of the tomb.
-    But the messenger who was supposed to deliver the message to Romeo is murdered.
-    When Romeo learns that Juliet is supposedly dead, he drinks the poison and dies next to Juliet's body.
-    After a while, Juliet wakes up next to the body of her love and stabs herself with Romeo's dagger.
-    Through this feud, the families become aware of their actions and reconcile with each other.
-    As a token of their mutual respect, the families build golden statues of Romeo and Juliet.
+The story is about two descendants of Romeo and Juliet from families that hate each other - the Capulets and the Montagues.
+It all starts with a ball organised by the Capulets, at which Juliet is to be betrothed to Paride.
+Romeo hears the news and goes there. At the ball, however, he meets Tybalt.
+Tybalt tries to provoke him into an argument, but is unsuccessful.
+At the ball, Romeo meets Juliet, with whom he immediately falls in love.
+When the ball ends, Romeo decides to visit Juliet in the Capulets' garden.
+The next day, Tybalt kills Romeo's friend Mercutio and Romeo kills Tybalt in return.
+Romeo is then banished from Verona, where the story takes place.
+After some time, Romeo receives news that Juliet is going to marry the Parido.
+Juliet is not given a choice and is forced into the marriage by her clan, so she devises a plan.
+She therefore asks Father Lawrence to give her a potion, after drinking it she will look dead for a while and then Romeo will take her out of the tomb.
+But the messenger who was supposed to deliver the message to Romeo is murdered.
+When Romeo learns that Juliet is supposedly dead, he drinks the poison and dies next to Juliet's body.
+After a while, Juliet wakes up next to the body of her love and stabs herself with Romeo's dagger.
+Through this feud, the families become aware of their actions and reconcile with each other.
+As a token of their mutual respect, the families build golden statues of Romeo and Juliet.
     """,
     """
-    
+Romeo - Romeo is the main male character in William Shakespeare's play "Romeo and Juliet." He is a young nobleman of the Montague family, who are at feud with the Capulet family. Romeo is a passionate and impulsive character who is deeply in love with Juliet, the daughter of his family's enemy.
+he could be described with these adjectives: passionate, impulsive, romantic, tragic, idealistic, poetic
+Juliet - Juliet is the main female character in William Shakespeare's play "Romeo and Juliet." She is a young woman of the Capulet family, who are at feud with the Montague family. Juliet is a strong-willed and intelligent character who is deeply in love with Romeo, the son of her family's enemy.
+she could be described with these adjectives: strong willed, inteligent, passionate, tragic, idealistic
+    """,
     """
-
+Drama - tragedy
+    """,
+    """
+Verona, Italy
+16th century
+    """,
+    """
+Shakespeare treated the familiar theme of unhappy love, which inspired one of Ovid's stories in The Metamorphosis. Which shows that this is a Renaissance work, where the authors went back to antiquity.
+The motif originated in Italy
+    """,
+    """
+The play explores the destructive power of hatred and conflict, as the feud between the Montagues and the Capulets ultimately leads to the tragic deaths of Romeo and Juliet. This serves to emphasize the idea that love is a powerful force that can overcome hatred and conflict, and that it is important to strive for understanding and reconciliation rather than division and strife.
+    """,
+    """
+Dílo je velmi zajímavé. Jako zápor vidím popis lásky mezi Romeem a Julií, příjde mi to nereálné až přehnané. Kniha je plná krásné slovní zásoby a myšlenka díla je velmi silná.
+    """
 ]
