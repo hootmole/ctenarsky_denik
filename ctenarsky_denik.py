c329423226a1e2b873aedcd1f3645f506ac9b6d6
hootmole <jachym1137@gmail.com>
2022-12-30 15:46:12 +0100

    Update ctenarsky_denik.py

--- ctenarsky_denik.py
+++ ctenarsky_denik.py
@@ -1,1 +1,37 @@
-print("hello world")
+import requests
+import json
+import deepl
+import random
+import os
+import openai
+import math
+import cv2
+import numpy
+# base libraries
+
+
+# global functions
+def translateTo(text: str, target_lang="CS") -> str:
+    deepl_key = "674e3591-912f-797e-09bc-591d8631b111:fx"
+    translator = deepl.Translator(deepl_key)
+    result = translator.translate_text(text, target_lang=target_lang)
+    translated_funfact = result.text
+
+    return translated_funfact
+
+
+def openai_response(prompt: str, token_lenght=150, ) -> str:
+    openai.api_key = "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"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    return response['choices'][0]['text'].replace("\n", " ")
+
