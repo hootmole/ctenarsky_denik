8ef711cbb2d923bf42ff37c9eb3f3e4072732958
hootmole <jachym1137@gmail.com>
2022-12-31 20:34:54 +0100

    added bibliography

--- ctenarsky_denik.py
+++ ctenarsky_denik.py
@@ -46,11 +46,105 @@ def openai_response(prompt: str, token_lenght=150, ) -> str:
         presence_penalty=0
     )
 
-    return response['choices'][0]['text'].replace("\n", " ")
+    resp =  response['choices'][0]['text'].replace("\n", " ")
+    # for removing unvanted spaces in front of a response
+    for char in resp:
+        if char == " ":
+            resp = resp[1:]
+        else:
+            return resp
 
 
 
+book = "Hamlet"
+
+
+class bibliographic_data:
+    def __init__(self, LN) -> None:
+        self.LN = LN
+        self.max_words_person_name = 4 # insurance for checking if the name responses are valid
+        self.max_words_publisher_name = 5 # insurance for checking if the publisher response is valid
+        self.on_error_repeats = 3 # insurance for checking if the publisher response is valid
+        self.output = {
+            "Author": "",
+            "Illustrator": "",
+            "Translator": "",
+            "Publisher": "",
+            "Page_count": "",
+        }
+
+    def Author(self):
+        for _ in range(self.on_error_repeats):
+            data = openai_response(f"who is the author of {book}, respond with one name, nothing more.")
+            if data.count(" ") < self.max_words_person_name:
+                self.output["Author"] = data
+                return 1
+        print("There is problem with collecting book author name")
+        self.output["Author"] = "Unknown"
+        return -1
+
+    def Illustrator(self):
+        for _ in range(self.on_error_repeats):
+            data = openai_response(f"who is the illustrator of {book}, respond with one name, nothing more.")
+            if data.count(" ") < self.max_words_person_name:
+                self.output["Illustrator"] = data
+                return 1
+        print("There is problem with collecting book illustrator name")
+        self.output["Illustrator"] = "Unknown"
+        return -1
+
+    def Translator(self):
+        if not "EN" in self.LN:
+            for _ in range(self.on_error_repeats):
+                data = openai_response(f"who is the translator of {book} to {self.LN}, respond with one name, nothing more.")
+                if data.count(" ") < self.max_words_person_name and data != self.output["Author"]:
+                    self.output["Translator"] = data
+                    return 1
+            print("There is problem with collecting book translator name")
+            self.output["Translator"] = "Unknown"
+            return -1
+        else:
+            self.output["Translator"] = self.output["Author"]
+            return 1
+
+
+    def Publisher(self):
+        for _ in range(self.on_error_repeats):
+            data = openai_response(f"name one book publishing company that published {book} to {self.LN}, respond with one name only.")
+            if data.count(" ") < self.max_words_person_name:
+                self.output["Publisher"] = data
+                return 1
+        print("There is problem with collecting book publisher name")
+        self.output["Publisher"] = "Unknown"
+        return -1
+
+    def Page_Count(self):
+        for _ in range(self.on_error_repeats):
+            data = openai_response(f"give me number of pages of a {self.LN} {book} book, give me one number only, try to find books with lower number.")
+            try:
+                self.output["Page_count"] = int(data)
+                return 1
+            except ValueError:
+                continue
+        print("There is problem with collecting book page count")
+        self.output["Page_count"] = "Unknown"
+        return -1
+
+    def complete(self):
+        error_rate = [
+            self.Author(),
+            self.Illustrator(),
+            self.Translator(),
+            self.Publisher(),
+            self.Page_Count(),
+        ]
+        print(error_rate)
+        error_rate = (5 - sum(error_rate)) * 10
+        print("Error rate:", error_rate, "%")
+        return self.output
+        
 
 
 
-
+a = bibliographic_data("CS")
+print(a.complete())
